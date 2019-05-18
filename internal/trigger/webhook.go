0ea273fd1c7146848402b8006c4ed0c8f107a875
Vincent van Dam <vincentd@erg.verweg.com>
2019-05-18 12:40:04 +0200

    Add webhook implementation

--- internal/trigger/webhook.go
+++ internal/trigger/webhook.go
@@ -1,5 +1,17 @@
 package trigger
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+)
+
 // WebhookTrigger is the object that implements http based triggers.
 type WebhookTrigger struct {
 	config Config
@@ -26,6 +38,124 @@ func (s *WebhookTrigger) GetConfig() Config {
 
 // Execute will trigger the webhook.
 func (s *WebhookTrigger) Execute() error {
-	// TODO: call Webhook/handle timeout
+	cli, err := s.newClient()
+	if err != nil {
+		return err
+	}
+	req, err := s.newRequest()
+	if err != nil {
+		return err
+	}
+	resp, err := cli.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("error webhook; status=%s(%d)", resp.Status, resp.StatusCode)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	glog.V(5).Infof("url: %s, status: %d, body: %s", s.config["url"], resp.Status, body)
 	return nil
+}
+
+// newClient will create a new http.Client object with the correct settings, as
+// reflected in the config.
+func (s *WebhookTrigger) newClient() (*http.Client, error) {
+	timeout, err := s.getTimeout()
+	if err != nil {
+		return nil, err
+	}
+	return &http.Client{
+		Timeout: timeout,
+	}, nil
+}
+
+// newRequest will create a http.Request for the configured url, body and
+// method.
+func (s *WebhookTrigger) newRequest() (*http.Request, error) {
+	body := s.getBody()
+	method := s.getMethod()
+	url, err := s.getUrl()
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	headers, err := s.getHeaders()
+	if err != nil {
+		return nil, err
+	}
+	for headr, val := range headers {
+		req.Header.Set(headr, val)
+	}
+	return req, nil
+}
+
+func (s *WebhookTrigger) getUrl() (string, error) {
+	url := strings.TrimSpace(s.config["url"])
+	if url == "" {
+		return "", fmt.Errorf("no url specified")
+	}
+	return url, nil
+}
+
+// getBody will process the configured body, and return an io.ReadWriter for
+// that body.
+func (s *WebhookTrigger) getBody() io.ReadWriter {
+	buf := new(bytes.Buffer)
+	body := s.config["body"]
+	if body != "" {
+		buf.WriteString(body)
+	}
+	return buf
+}
+
+// getTimeout will return a time.Duration for the configured timeout. If no
+// timeout has been configured it will use a default timeout instead.
+func (s *WebhookTrigger) getTimeout() (time.Duration, error) {
+	to := s.config["timeout"]
+	if to == "" {
+		to = "300ms"
+	}
+	return time.ParseDuration(to)
+}
+
+// getMethod will return the method as configured. If no method is set, it will
+// default to GET if no body is configured, or POST if a body has been
+// configured.
+func (s *WebhookTrigger) getMethod() string {
+	method := strings.ToUpper(s.config["method"])
+	if method == "" {
+		method = "GET"
+		if s.config["body"] != "" {
+			method = "POST"
+		}
+	}
+	return method
+}
+
+// getHeaders will parse the headers configuration, and return a map containing
+// the headers and its' values.
+func (s *WebhookTrigger) getHeaders() (map[string]string, error) {
+	headers := map[string]string{}
+	chdrs := strings.Split(strings.Replace(s.config["headers"], "\r\n", "\n", -1), "\n")
+	for _, header := range chdrs {
+		if header == "" {
+			continue
+		}
+		flds := strings.Split(header, ":")
+		if len(flds) != 2 {
+			return nil, fmt.Errorf("invalid header specified '%s'", header)
+		}
+		flds[0] = strings.TrimSpace(flds[0])
+		flds[1] = strings.TrimSpace(flds[1])
+		if flds[0] == "" || flds[1] == "" {
+			continue
+		}
+		headers[flds[0]] = flds[1]
+	}
+	return headers, nil
 }
---
+++ internal/trigger/webhook_test.go
@@ -0,0 +1,195 @@
+package trigger
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestNewWebhookTrigger(t *testing.T) {
+	wht, err := New("webhook")
+	if err != nil {
+		t.Errorf("failed test - could not instantiate webhook module; %s", err)
+	}
+	in := Config{
+		"test1": "value1",
+		"test2": "value2",
+	}
+	wht.SetConfig(in)
+	out := wht.GetConfig()
+	if !reflect.DeepEqual(in, out) {
+		t.Errorf("failed test - configuration not correctly set; expected %v, got %v", in, out)
+	}
+}
+
+func TestTimeout(t *testing.T) {
+	tests := []struct {
+		cfg      Config
+		duration time.Duration
+		err      bool
+	}{
+		{
+			cfg:      Config{},
+			duration: 300 * time.Millisecond,
+			err:      false,
+		},
+		{
+			cfg: Config{
+				"timeout": "1s",
+			},
+			duration: 1 * time.Second,
+			err:      false,
+		},
+		{
+			cfg: Config{
+				"timeout": "just wait for 1 second",
+			},
+			duration: 0,
+			err:      true,
+		},
+	}
+
+	wht := &WebhookTrigger{}
+	for i, tst := range tests {
+		wht.SetConfig(tst.cfg)
+		dur, err := wht.getTimeout()
+		if err != nil && !tst.err {
+			t.Errorf("failed test %d - unexpected err when getTimeout: %s", i, err)
+		}
+		if err == nil && tst.err {
+			t.Errorf("failed test %d - expected err when getTimeout, but got none", i)
+		}
+		if err == nil && tst.duration != dur {
+			t.Errorf("failed test %d - expected %s, but got %s", i, tst.duration, dur)
+		}
+		cli, err := wht.newClient()
+		if err != nil && !tst.err {
+			t.Errorf("failed test %d - unexpected err when newClient: %s", i, err)
+		}
+		if err == nil && tst.err {
+			t.Errorf("failed test %d - expected err when newClient, but got none", i)
+		}
+		if err == nil && tst.duration != cli.Timeout {
+			t.Errorf("failed test %d - expected %s in http.Client, but got %s", i, tst.duration, dur)
+		}
+	}
+}
+
+func TestRequest(t *testing.T) {
+	tests := []struct {
+		cfg    Config
+		method string
+		err    bool
+	}{
+		{
+			cfg:    Config{},
+			method: "GET",
+			err:    true,
+		},
+		{
+			cfg: Config{
+				"url": "http://localhost:8080",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":  "http://localhost:8080",
+				"body": "post this for me",
+			},
+			method: "POST",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":    "http://localhost:8080",
+				"method": "POST",
+				"body":   "post this for me",
+			},
+			method: "POST",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":    "http://localhost:8080",
+				"method": "GET",
+				"body":   "method will override, even if body is present",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":    "http://localhost:8080",
+				"method": "UPDATE",
+				"body":   "update this for me",
+			},
+			method: "UPDATE",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":    "http://localhost:8080",
+				"method": "Update",
+			},
+			method: "UPDATE",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":     "http://localhost:8080",
+				"headers": "Content-type: application/json",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":     "http://localhost:8080",
+				"headers": "Content-type: ",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":     "http://localhost:8080",
+				"headers": " : ",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":     "http://localhost:8080",
+				"headers": "Content-type: application/json\nAuthorisation: something",
+			},
+			method: "GET",
+			err:    false,
+		},
+		{
+			cfg: Config{
+				"url":     "http://localhost:8080",
+				"headers": "Content-type",
+			},
+			method: "GET",
+			err:    true,
+		},
+	}
+
+	wht := &WebhookTrigger{}
+	for i, tst := range tests {
+		wht.SetConfig(tst.cfg)
+		req, err := wht.newRequest()
+		if err != nil && !tst.err {
+			t.Errorf("failed test %d - unexpected err when newRequest: %s", i, err)
+		}
+		if err == nil && tst.err {
+			t.Errorf("failed test %d - expected err when newRequest, but got none", i)
+		}
+		if err == nil && tst.method != req.Method {
+			t.Errorf("failed test %d - expected %s in http.Request, but got %s", i, tst.method, req.Method)
+		}
+	}
+}
